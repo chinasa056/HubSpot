28f6638487e3b39cd3a95992bbe14965fe560686
chinasa056 <chinasaacha05@gmail.com>
2025-03-30 21:09:12 +0100

    updated some functions

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,20 @@
-const{register} = require('../controllers/userController')
+const{register, verifyUser, login, forgottenPassword, resetPassword, changePassword, loggedOut} = require('../controllers/userController')
 
 const router = require('express').Router(); 
+
 router.post('/register-user', register);
 
+router.get("/user/verify", verifyUser);
+
+router.post("/user/login", login);
+
+router.post("/user/forgot-password", forgottenPassword);
+
+router.post("/users/reset-password/:token", resetPassword);
+
+router.patch("/users/change-password/:userId", changePassword);
+
+router.patch("/users/logout", loggedOut);
 
 
 module.exports = router;
