aee4f6b2b97ba4e87fb6b0b0f6d9f2c172c2c361
chinasa056 <chinasaacha05@gmail.com>
2025-04-17 23:31:57 +0100

    added space listing validator

--- middleware/validator.js
+++ middleware/validator.js
@@ -235,8 +235,7 @@ exports.addSpaceValidator = (req, res, next) => {
 
   if (error) {
     return res.status(400).json({
-      message: "Validation errors occurred.",
-      errors: error.details.map((detail) => detail.message),
+      message: error.details.map((detail) => detail.message),
     });
   }
 
